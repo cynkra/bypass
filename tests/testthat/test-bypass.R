bf5cff5bd63d8a218569f52505818f8c0f7e1ff4
Antoine Fabri <antoine.fabri@gmail.com>
2024-05-30 18:40:38 +0200

    simplify test

--- tests/testthat/test-bypass.R
+++ tests/testthat/test-bypass.R
@@ -2,19 +2,14 @@ test_that("bypass", {
   x <- structure(list(a = 1, b = 2), class = "foo")
   c.foo <- function(...) 42
   dim.foo <- function(...) 42
-  #`dim<-.foo` <- function(...) 42
   `$.foo` <- function(...) 42
-  #`$<-.foo` <- function(...) 42
   lapply.foo <- function(...) 42
   length.foo <- function(...) 42
   lengths.foo <- function(...) 42
   names.foo <- function(...) 42
-  #`names<-.foo` <- function(...) 42
   sapply.foo <- function(...) 42
   `[.foo` <- function(...) 42
   `[[.foo` <- function(...) 42
-  # `[<-.foo` <- function(...) 42
-  # `[[<-.foo` <- function(...) 42
   unlist.foo <- function(...) 42
 
   expect_snapshot({
